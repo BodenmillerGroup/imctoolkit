5b6184a059558053ee8cfbd5d257d7b65c0aafec
Jonas Windhager <jonas.windhager@uzh.ch>
2020-11-06 22:50:28 +0100

    fix __all__ in __init__ to include utils module

--- imctoolkit/__init__.py
+++ imctoolkit/__init__.py
@@ -1,5 +1,6 @@
+from . import utils
 from .multichannel_image import MultichannelImage
 from .single_cell_data import SingleCellData
 from .spatial_cell_graph import SpatialCellGraph
 
-__all__ = ['MultichannelImage', 'SingleCellData', 'SpatialCellGraph']
+__all__ = ['MultichannelImage', 'SingleCellData', 'SpatialCellGraph', 'utils']
