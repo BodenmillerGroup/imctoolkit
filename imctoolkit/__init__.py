c203ddc9a74dd726e24036a66353964c9fa6ddfb
Jonas Windhager <jonas.windhager@uzh.ch>
2020-11-10 14:47:44 +0100

    fixed __init__.py

--- imctoolkit/__init__.py
+++ imctoolkit/__init__.py
@@ -1,6 +1,11 @@
+from imctoolkit import image, single_cell_data, spatial_cell_graph
 from imctoolkit.image.multichannel_image import MultichannelImage
 from imctoolkit.single_cell_data.spatial_single_cell_data import SpatialSingleCellData
 from imctoolkit.single_cell_data.image_single_cell_data import ImageSingleCellData
 from imctoolkit.spatial_cell_graph.spatial_cell_graph import SpatialCellGraph
 
-__all__ = ['MultichannelImage', 'SpatialSingleCellData', 'ImageSingleCellData', 'SpatialCellGraph']
+__all__ = [
+    'image', 'MultichannelImage',
+    'single_cell_data', 'SpatialSingleCellData', 'ImageSingleCellData',
+    'spatial_cell_graph', 'SpatialCellGraph'
+]
